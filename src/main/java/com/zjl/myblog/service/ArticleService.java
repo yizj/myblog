b6dff093ac0b4ff348e1fff56b05102d4f08bb31
jlzhang <jlzhang@travelsky.com>
2019-11-11 16:57:18 +0800

    添加文章发布

---
+++ src/main/java/com/zjl/myblog/service/ArticleService.java
@@ -0,0 +1,9 @@
+package com.zjl.myblog.service;
+
+import com.zjl.myblog.domain.Article;
+
+
+public interface ArticleService {
+
+    public Article addArticle(Article article) throws Exception;
+}
---
+++ src/main/java/com/zjl/myblog/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,24 @@
+package com.zjl.myblog.service.impl;
+
+import com.zjl.myblog.domain.Article;
+import com.zjl.myblog.repository.ArticleRepository;
+import com.zjl.myblog.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleServiceImpl implements ArticleService {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Override
+    public Article addArticle(Article article) throws Exception {
+        Article resArticle=articleRepository.save(article);
+        if(resArticle==null)
+        {
+            throw new Exception("发布文章失败!");
+        }
+        return resArticle;
+    }
+}
